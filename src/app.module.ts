01395193bd40c7d568f51dcda2515040e26989bf
ricardojunior02 <ricardojunior199502@gmail.com>
2021-06-07 22:02:52 -0300

    App module

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CreateUserController } from './create-user/create-user.controller';
+import { SendMailConsumer } from './jobs/sendMail-consumer';
+import { SendMailProducerService } from './jobs/sendMail-producer-service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'sendMail-queue',
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      },
+    }),
+  ],
+  controllers: [CreateUserController],
+  providers: [SendMailProducerService, SendMailConsumer],
+})
+export class AppModule {}
