753f3d7dbc78d9dbe7d04279b906c1b760e159ef
ricardojunior02 <ricardojunior199502@gmail.com>
2021-06-07 22:12:20 -0300

    Add Dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { MailerModule } from '@nestjs-modules/mailer';
-import { BullModule } from '@nestjs/bull';
+import { BullModule, InjectQueue } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MiddlewareBuilder } from '@nestjs/core';
+import { Queue } from 'bull';
 import { CreateUserController } from './create-user/create-user.controller';
 import { SendMailConsumer } from './jobs/sendMail-consumer';
 import { SendMailProducerService } from './jobs/sendMail-producer-service';
+import { createBullBoard } from 'bull-board';
+import { BullAdapter } from 'bull-board/bullAdapter';
 
 @Module({
   imports: [
@@ -32,4 +36,12 @@ import { SendMailProducerService } from './jobs/sendMail-producer-service';
   controllers: [CreateUserController],
   providers: [SendMailProducerService, SendMailConsumer],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(@InjectQueue('sendMail-queue') private queue: Queue) {}
+
+  configure(consumer: MiddlewareBuilder) {
+    const { router } = createBullBoard([new BullAdapter(this.queue)]);
+
+    consumer.apply(router).forRoutes('admin/queues');
+  }
+}
